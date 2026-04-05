9d08f84c0edc1dee34b3b457805af375a6f7a416
tturner <tturner@bastiansolutions.com>
2026-04-05 09:21:17 -0500

    update

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './home.page.html',
 })
-export class HomePage {}
+export class HomePage {
+    scrollToAreasOfFocus(): void {
+        const el = document.getElementById('focus');
+        if (!el) return;
+
+        const headerOffset = 30;
+        const elementPosition = el.getBoundingClientRect().top + window.scrollY;
+        const offsetPosition = elementPosition - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth',
+        });
+    }
+}
