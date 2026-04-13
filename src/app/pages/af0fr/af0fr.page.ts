333a972f1c7261968ba555a9ffc0d3636b1d1ef3
tturner <tturner@bastiansolutions.com>
2026-04-13 13:03:47 -0500

    refactor

--- src/app/pages/af0fr/af0fr.page.ts
+++ src/app/pages/af0fr/af0fr.page.ts
@@ -1,45 +1,17 @@
 import { Component } from '@angular/core';
-import * as htmlToImage from 'html-to-image';
+import {HamIntro} from "./ham_intro/ham_intro.component";
+import {HamQSL} from "./ham_qsl/ham_qsl.component";
+import {AntennaBuild} from "./antenna-build/antenna-build.component";
+import {EchoLink} from "./echo_link/echolink.component";
 
 @Component({
     standalone: true,
     templateUrl: './af0fr.page.html',
+    imports: [
+        HamIntro,
+        HamQSL,
+        AntennaBuild,
+        EchoLink
+    ]
 })
-export class Af0frPage {
-    scrollToAntennaBuild(): void {
-        const el = document.getElementById('antenna-build');
-        if (!el) return;
-
-        const headerOffset = 88;
-        const elementPosition = el.getBoundingClientRect().top + window.scrollY;
-        const offsetPosition = elementPosition - headerOffset;
-
-        window.scrollTo({
-            top: offsetPosition,
-            behavior: 'smooth',
-        });
-    }
-
-    downloadCard(): void {
-        const node = document.getElementById('contact-card-export');
-        if (!node) return;
-
-        htmlToImage.toPng(node, {
-            pixelRatio: 2,
-            backgroundColor: '#f97316',
-            cacheBust: true,
-            style: {
-                padding: '20px',
-            }
-        })
-            .then((dataUrl) => {
-                const link = document.createElement('a');
-                link.download = 'AF0FR-contact.png';
-                link.href = dataUrl;
-                link.click();
-            })
-            .catch((err) => {
-                console.error('Error generating image:', err);
-            });
-    }
-}
+export class Af0frPage {}
---
+++ src/app/pages/af0fr/antenna-build/antenna-build.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'antenna-build',
+    standalone: true,
+    templateUrl: './antenna-build.component.html',
+})
+export class AntennaBuild {}
