c1be9f7d863fc4fd63f31e6a2217d0fc896a50dc
tturner <tturner@bastiansolutions.com>
2026-05-08 17:26:39 -0500

    project selector

--- src/app/pages/af0fr/ham_intro/ham_intro.component.ts
+++ src/app/pages/af0fr/ham_intro/ham_intro.component.ts
@@ -34,6 +34,34 @@ export class HamIntro {
         });
     }
 
+    scrollToKeyerBuild(): void {
+        const el = document.getElementById('keyer');
+        if (!el) return;
+
+        const headerOffset = 88;
+        const elementPosition = el.getBoundingClientRect().top + window.scrollY;
+        const offsetPosition = elementPosition - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth',
+        });
+    }
+
+    scrollToEFHWBuild(): void {
+        const el = document.getElementById('antenna-build-efhw');
+        if (!el) return;
+
+        const headerOffset = 88;
+        const elementPosition = el.getBoundingClientRect().top + window.scrollY;
+        const offsetPosition = elementPosition - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth',
+        });
+    }
+
     openQrz() {
         window.open('https://www.qrz.com/db/AF0FR', '_blank');
     }
